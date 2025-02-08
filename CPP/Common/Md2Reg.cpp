a45dc3059932e13cb79bb23d9da01f8bfcaa96ad
cielavenir <cielartisan@gmail.com>
2025-02-09 01:34:10 +0900

    Migrate Md2 Hasher

--- CPP/Common/Md2Reg.cpp
+++ CPP/Common/Md2Reg.cpp
@@ -11,6 +11,11 @@ EXTERN_C_END
 #include "../Common/MyCom.h"
 #include "../7zip/Common/RegisterCodec.h"
 
+#include "../../C/7zVersion.h"
+#if MY_VER_MAJOR >= 23
+#define MY_UNKNOWN_IMP1 Z7_COM_UNKNOWN_IMP_1
+#endif
+
 // MD2
 class CMD2Hasher:
   public IHasher,
@@ -23,7 +28,14 @@ public:
   CMD2Hasher() { MD2_Init(&_ctx); }
 
   MY_UNKNOWN_IMP1(IHasher)
+#if MY_VER_MAJOR >= 23
+  Z7_IFACE_COM7_IMP(IHasher)
+#else
   INTERFACE_IHasher(;)
+#endif
+
+public:
+  virtual ~CMD2Hasher() = default;
 };
 
 STDMETHODIMP_(void) CMD2Hasher::Init() throw()
