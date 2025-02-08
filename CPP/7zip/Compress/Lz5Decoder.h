8ef119153d3bbe68a5a60af286b31d7b7245bd34
cielavenir <cielartisan@gmail.com>
2025-02-09 08:30:12 +0900

    Migrate Lz5 Codec

--- CPP/7zip/Compress/Lz5Decoder.h
+++ CPP/7zip/Compress/Lz5Decoder.h
@@ -14,6 +14,17 @@
 #include "../Common/RegisterCodec.h"
 #include "../Common/ProgressMt.h"
 
+#include "../../../C/7zVersion.h"
+#if MY_VER_MAJOR >= 23
+#define OVERRIDE override
+#define MY_QUERYINTERFACE_BEGIN2 Z7_COM_QI_BEGIN2
+#define MY_QUERYINTERFACE_ENTRY Z7_COM_QI_ENTRY
+#define MY_QUERYINTERFACE_END Z7_COM_QI_END
+#define MY_ADDREF_RELEASE Z7_COM_ADDREF_RELEASE
+#else
+#define OVERRIDE
+#endif
+
 struct Lz5Stream
 {
   ISequentialInStream *inStream;
@@ -50,6 +61,9 @@ namespace NCompress
 
     class CDecoder : public ICompressCoder,
                      public ICompressSetDecoderProperties2,
+                     #ifndef NO_READ_FROM_CODER
+                     public ICompressSetInStream,
+                     #endif
                      public ICompressSetCoderMt,
                      public CMyUnknownImp
     {
@@ -73,22 +87,23 @@ namespace NCompress
 #endif
       MY_QUERYINTERFACE_ENTRY(ICompressSetCoderMt)
       MY_QUERYINTERFACE_END
-
       MY_ADDREF_RELEASE
+
+    public:
       STDMETHOD(Code)
-      (ISequentialInStream *inStream, ISequentialOutStream *outStream, const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress);
+      (ISequentialInStream *inStream, ISequentialOutStream *outStream, const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress) noexcept OVERRIDE;
       STDMETHOD(SetDecoderProperties2)
-      (const Byte *data, UInt32 size);
+      (const Byte *data, UInt32 size) noexcept OVERRIDE;
       STDMETHOD(SetOutStreamSize)
-      (const UInt64 *outSize);
+      (const UInt64 *outSize) noexcept OVERRIDE;
       STDMETHOD(SetNumberOfThreads)
-      (UInt32 numThreads);
+      (UInt32 numThreads) noexcept OVERRIDE;
 
 #ifndef NO_READ_FROM_CODER
       STDMETHOD(SetInStream)
-      (ISequentialInStream *inStream);
+      (ISequentialInStream *inStream) noexcept OVERRIDE;
       STDMETHOD(ReleaseInStream)
-      ();
+      () noexcept OVERRIDE;
       UInt64 GetInputProcessedSize() const { return _processedIn; }
 #endif
       HRESULT CodeResume(ISequentialOutStream *outStream, const UInt64 *outSize, ICompressProgressInfo *progress);
