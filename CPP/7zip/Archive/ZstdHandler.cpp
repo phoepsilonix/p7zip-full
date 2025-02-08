45e23b6dc3d7d737805a4e3e48dce4a9b64c33b5
cielavenir <cielartisan@gmail.com>
2025-02-09 01:35:51 +0900

    Migrate Zstd Handler

--- CPP/7zip/Archive/ZstdHandler.cpp
+++ CPP/7zip/Archive/ZstdHandler.cpp
@@ -17,6 +17,16 @@
 #include "Common/DummyOutStream.h"
 #include "Common/HandlerOut.h"
 
+#include "../../../C/7zVersion.h"
+#if MY_VER_MAJOR >= 23
+#define OVERRIDE override
+#define NOEXCEPT_23ONLY noexcept
+#define MY_UNKNOWN_IMP4 Z7_COM_UNKNOWN_IMP_4
+#else
+#define OVERRIDE
+#define NOEXCEPT_23ONLY
+#endif
+
 using namespace NWindows;
 
 namespace NArchive {
@@ -52,12 +62,20 @@ public:
       IArchiveOpenSeq,
       IOutArchive,
       ISetProperties)
+#if MY_VER_MAJOR >= 23
+  Z7_IFACE_COM7_IMP(IInArchive)
+  Z7_IFACE_COM7_IMP(IOutArchive)
+#else
   INTERFACE_IInArchive(;)
   INTERFACE_IOutArchive(;)
-  STDMETHOD(OpenSeq)(ISequentialInStream *stream);
-  STDMETHOD(SetProperties)(const wchar_t * const *names, const PROPVARIANT *values, UInt32 numProps);
+#endif
+
+public:
+  STDMETHOD(OpenSeq)(ISequentialInStream *stream) noexcept OVERRIDE;
+  STDMETHOD(SetProperties)(const wchar_t * const *names, const PROPVARIANT *values, UInt32 numProps) noexcept OVERRIDE;
 
   CHandler() { }
+  virtual ~CHandler() = default;
 };
 
 static const Byte kProps[] =
@@ -75,18 +93,18 @@ static const Byte kArcProps[] =
 IMP_IInArchive_Props
 IMP_IInArchive_ArcProps
 
-STDMETHODIMP CHandler::GetArchiveProperty(PROPID /*propID*/, PROPVARIANT * /*value*/)
+STDMETHODIMP CHandler::GetArchiveProperty(PROPID /*propID*/, PROPVARIANT * /*value*/) NOEXCEPT_23ONLY
 {
   return S_OK;
 }
 
-STDMETHODIMP CHandler::GetNumberOfItems(UInt32 *numItems)
+STDMETHODIMP CHandler::GetNumberOfItems(UInt32 *numItems) NOEXCEPT_23ONLY
 {
   *numItems = 1;
   return S_OK;
 }
 
-STDMETHODIMP CHandler::GetProperty(UInt32 /* index */, PROPID propID, PROPVARIANT *value)
+STDMETHODIMP CHandler::GetProperty(UInt32 /* index */, PROPID propID, PROPVARIANT *value) NOEXCEPT_23ONLY
 {
   NCOM::CPropVariant prop;
   switch (propID)
@@ -136,7 +154,7 @@ API_FUNC_static_IsArc IsArc_zstd(const Byte *p, size_t size)
 /*
   解码打开 需要判断 IsArc_zstd
  */
-STDMETHODIMP CHandler::Open(IInStream *stream, const UInt64 *, IArchiveOpenCallback *)
+STDMETHODIMP CHandler::Open(IInStream *stream, const UInt64 *, IArchiveOpenCallback *) NOEXCEPT_23ONLY
 {
   COM_TRY_BEGIN
   Close();
@@ -157,7 +175,7 @@ STDMETHODIMP CHandler::Open(IInStream *stream, const UInt64 *, IArchiveOpenCallb
 /*
  打开 stream 不判断 IsArc_zstd 应该为编码打开
  */
-STDMETHODIMP CHandler::OpenSeq(ISequentialInStream *stream)
+STDMETHODIMP CHandler::OpenSeq(ISequentialInStream *stream) noexcept
 {
   Close();
   _isArc = true;
@@ -167,7 +185,7 @@ STDMETHODIMP CHandler::OpenSeq(ISequentialInStream *stream)
 /*
  关闭 handler 所有属性复位
  */
-STDMETHODIMP CHandler::Close()
+STDMETHODIMP CHandler::Close() NOEXCEPT_23ONLY
 {
   _isArc = false;
   _dataAfterEnd = false;
@@ -186,7 +204,7 @@ STDMETHODIMP CHandler::Close()
  解码 接口
  */
 STDMETHODIMP CHandler::Extract(const UInt32 *indices, UInt32 numItems,
-    Int32 testMode, IArchiveExtractCallback *extractCallback)
+    Int32 testMode, IArchiveExtractCallback *extractCallback) NOEXCEPT_23ONLY
 {
   COM_TRY_BEGIN
   if (numItems == 0)
@@ -304,7 +322,7 @@ static HRESULT UpdateArchive(
   return updateCallback->SetOperationResult(NArchive::NUpdate::NOperationResult::kOK);
 }
 
-STDMETHODIMP CHandler::GetFileTimeType(UInt32 *type)
+STDMETHODIMP CHandler::GetFileTimeType(UInt32 *type) NOEXCEPT_23ONLY
 {
   *type = NFileTimeType::kUnix;
   return S_OK;
@@ -313,7 +331,7 @@ STDMETHODIMP CHandler::GetFileTimeType(UInt32 *type)
  调用 UpdateArchive 更新项目
  */
 STDMETHODIMP CHandler::UpdateItems(ISequentialOutStream *outStream, UInt32 numItems,
-    IArchiveUpdateCallback *updateCallback)
+    IArchiveUpdateCallback *updateCallback) NOEXCEPT_23ONLY
 {
   COM_TRY_BEGIN
 
@@ -374,7 +392,7 @@ STDMETHODIMP CHandler::UpdateItems(ISequentialOutStream *outStream, UInt32 numIt
   COM_TRY_END
 }
 
-STDMETHODIMP CHandler::SetProperties(const wchar_t * const *names, const PROPVARIANT *values, UInt32 numProps)
+STDMETHODIMP CHandler::SetProperties(const wchar_t * const *names, const PROPVARIANT *values, UInt32 numProps) noexcept
 {
   return _props.SetProperties(names, values, numProps);
 }
