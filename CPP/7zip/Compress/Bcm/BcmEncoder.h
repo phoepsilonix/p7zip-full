23403e679f0ca3f6cfeb9285d3f6cfbdcb9ceefa
cielavenir <cielartisan@gmail.com>
2025-02-09 08:29:07 +0900

    Migrate BCM Codec

--- CPP/7zip/Compress/Bcm/BcmEncoder.h
+++ CPP/7zip/Compress/Bcm/BcmEncoder.h
@@ -10,6 +10,15 @@
 #include "../../Common/InBuffer.h"
 #include "../../Common/OutBuffer.h"
 
+#include "../../../../C/7zVersion.h"
+#if MY_VER_MAJOR >= 23
+#define OVERRIDE override
+#define MY_UNKNOWN_IMP1 Z7_COM_UNKNOWN_IMP_1
+#define MY_UNKNOWN_IMP2 Z7_COM_UNKNOWN_IMP_2
+#else
+#define OVERRIDE
+#endif
+
 namespace NCompress {
 namespace NBCM {
 
@@ -34,12 +43,14 @@ public:
 
   MY_UNKNOWN_IMP2(ICompressGetInStreamProcessedSize, ICompressSetCoderProperties)
 
+public:
   STDMETHOD(Code)(ISequentialInStream *inStream, ISequentialOutStream *outStream,
-      const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress);
-  STDMETHOD (SetCoderProperties)(const PROPID *propIDs, const PROPVARIANT *props, UInt32 numProps);
-  STDMETHOD(GetInStreamProcessedSize)(UInt64 *value);
+      const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress) noexcept OVERRIDE;
+  STDMETHOD (SetCoderProperties)(const PROPID *propIDs, const PROPVARIANT *props, UInt32 numProps) noexcept OVERRIDE;
+  STDMETHOD(GetInStreamProcessedSize)(UInt64 *value) noexcept OVERRIDE;
 
   CEncoder(): level(4){}
+  virtual ~CEncoder() = default;
 };
 
 class CDecoder:
@@ -60,11 +71,13 @@ public:
 
   MY_UNKNOWN_IMP1(ICompressGetInStreamProcessedSize)
 
+public:
   STDMETHOD(Code)(ISequentialInStream *inStream, ISequentialOutStream *outStream,
-      const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress);
-  STDMETHOD(GetInStreamProcessedSize)(UInt64 *value);
+      const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress) noexcept OVERRIDE;
+  STDMETHOD(GetInStreamProcessedSize)(UInt64 *value) noexcept OVERRIDE;
 
   CDecoder(){}
+  virtual ~CDecoder() = default;
 };
 
 }}
--- CPP/7zip/Compress/Bcm/BcmRegister.cpp
+++ CPP/7zip/Compress/Bcm/BcmRegister.cpp
@@ -474,7 +474,7 @@ HRESULT CDecoder::CodeReal(ISequentialInStream *inStream, ISequentialOutStream *
 
 
 STDMETHODIMP CDecoder::Code(ISequentialInStream *inStream, ISequentialOutStream *outStream,
-    const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress)
+    const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress) noexcept
 {
   try { return CodeReal(inStream, outStream, inSize, outSize, progress);  }
   catch(const CInBufferException &e)  { return e.ErrorCode; }
@@ -482,13 +482,13 @@ STDMETHODIMP CDecoder::Code(ISequentialInStream *inStream, ISequentialOutStream 
   catch(...) { return S_FALSE; }
 }
 
-STDMETHODIMP CDecoder::GetInStreamProcessedSize(UInt64 *value)
+STDMETHODIMP CDecoder::GetInStreamProcessedSize(UInt64 *value) noexcept
 {
   *value = processedIn;
   return S_OK;
 }
 
-STDMETHODIMP CEncoder::SetCoderProperties(const PROPID * propIDs, const PROPVARIANT * coderProps, UInt32 numProps)
+STDMETHODIMP CEncoder::SetCoderProperties(const PROPID * propIDs, const PROPVARIANT * coderProps, UInt32 numProps) noexcept
 {
   for (UInt32 i = 0; i < numProps; i++){
 	const PROPVARIANT & prop = coderProps[i];
@@ -525,7 +525,7 @@ HRESULT CEncoder::CodeReal(ISequentialInStream *inStream, ISequentialOutStream *
 
 
 STDMETHODIMP CEncoder::Code(ISequentialInStream *inStream, ISequentialOutStream *outStream,
-    const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress)
+    const UInt64 *inSize, const UInt64 *outSize, ICompressProgressInfo *progress) noexcept
 {
   try { return CodeReal(inStream, outStream, inSize, outSize, progress);  }
   catch(const CInBufferException &e)  { return e.ErrorCode; }
@@ -533,7 +533,7 @@ STDMETHODIMP CEncoder::Code(ISequentialInStream *inStream, ISequentialOutStream 
   catch(...) { return S_FALSE; }
 }
 
-STDMETHODIMP CEncoder::GetInStreamProcessedSize(UInt64 *value)
+STDMETHODIMP CEncoder::GetInStreamProcessedSize(UInt64 *value) noexcept
 {
   *value = processedIn;
   return S_OK;
