f3e707630e8912dfa9a24d915d3c58d80e470b43
cielavenir <cielartisan@gmail.com>
2025-02-09 01:34:14 +0900

    Migrate Md4 Hasher

--- CPP/Common/Md4Reg.cpp
+++ CPP/Common/Md4Reg.cpp
@@ -11,6 +11,11 @@ EXTERN_C_END
 #include "../Common/MyCom.h"
 #include "../7zip/Common/RegisterCodec.h"
 
+#include "../../C/7zVersion.h"
+#if MY_VER_MAJOR >= 23
+#define MY_UNKNOWN_IMP1 Z7_COM_UNKNOWN_IMP_1
+#endif
+
 // MD4
 class CMD4Hasher:
   public IHasher,
@@ -23,7 +28,14 @@ public:
   CMD4Hasher() { MD4_Init(&_ctx); }
 
   MY_UNKNOWN_IMP1(IHasher)
+#if MY_VER_MAJOR >= 23
+  Z7_IFACE_COM7_IMP(IHasher)
+#else
   INTERFACE_IHasher(;)
+#endif
+
+public:
+  virtual ~CMD4Hasher() = default;
 };
 
 STDMETHODIMP_(void) CMD4Hasher::Init() throw()
